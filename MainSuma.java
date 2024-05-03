5bea232746deb49b8dbe6d417c04989c0daa90d3
NeruChan2003 <167128377+NeruChan2003@users.noreply.github.com>
2024-05-02 20:35:13 -0600

    Suma Version 2
    Suma de dos Numeros Version 2

--- MainSuma.java
+++ MainSuma.java
@@ -2,13 +2,31 @@ import java.util.Scanner;
 
 public class MainSuma {
     public static void main(String[] args) {
-        Scanner n1 = new Scanner(System.in);
-        System.out.println("Ingresa un numero");
-        int a = n1.nextInt();
-        System.out.println("Ingrese el segundo numero");
-        int b = n1.nextInt();
-        int r = a + b;
-        System.out.println("El resultado es:" + r);
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = input.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = input.nextInt();
+
+        // Crear una instancia de la clase Suma
+        Suma suma = new Suma();
+
+        // Establecer los valores de a y b en la instancia de Suma
+        suma.setA(a);
+        suma.setB(b);
+
+        // Realizar la suma usando el método sumar de la instancia de Suma
+        suma.sumar();
+
+        // Obtener el resultado de la instancia de Suma
+        int r = suma.getR();
+
+        System.out.println("El resultado es: " + r);
+
+        // Mostrar la representación de la instancia de Suma
+        System.out.println(suma.toString());
     }
 }
--- Suma.java
+++ Suma.java
@@ -1,8 +1,32 @@
 public class Suma {
-    int a, b, r;
+    private int a, b, r;
 
-    public int restar(int a, int b) {
-        r = this.a - this.b;
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getR() {
         return r;
+    }
+
+    public void sumar() {
+        r = a + b;
+    }
+
+    @Override
+    public String toString() {
+        return "Suma [a=" + a + ", b=" + b + ", r=" + r + "]";
     }
 }
